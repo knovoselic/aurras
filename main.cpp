e5e232ae0a2663f58e816b0d98e8dbfc7ebc1278
Kristijan Novoselić <kristijan@codecons.com>
2022-01-04 20:30:35 +0100

    Implement command line parser and option to switch mute state of all inputs

--- main.cpp
+++ main.cpp
@@ -22,31 +22,54 @@ void handle_signals(int signal) {
     qApp->quit();
 }
 
+int client_main(RunGuard &guard, QCoreApplication &app) {
+    QCommandLineParser parser;
+    parser.setApplicationDescription("Aurras driver.\nTo start the daemon, "
+                                     "run without any options. If the daemon "
+                                     "is already running, use the one of the "
+                                     "available options to send a command to the daemon.");
+    parser.addHelpOption();
+    parser.addVersionOption();
+
+    parser.addOptions({
+        {{"t", "toggle-mute"}, QCoreApplication::translate("main", "Toggle mute state of all input devices")}
+    });
+    parser.process(app);
+
+    if (parser.isSet("toggle-mute")) {
+        guard.writeToSharedMemory(RunGuard::ipc_commands::TOGGLE_MUTE);
+        return 0;
+    }
+
+    qCritical() << "Another instance is already running!\n";
+    parser.showHelp();
+    return 1;
+}
+
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    QCoreApplication app(argc, argv);
     RunGuard guard(SHARED_MEMORY_KEY);
+
+    app.setApplicationName("Aurras");
+    app.setApplicationVersion("v0.1");
+    if (!guard.initialize()) {
+        return client_main(guard, app);
+    }
+
+    PulseAudio pa;
+    KeyboardDriver keyboard;
 
     QObject::connect(&guard, &RunGuard::commandReceived, [](RunGuard::ipc_commands command) {
         qDebug() << "Command received:" << command;
     });
 
-    if (!guard.initialize()) {
-        guard.writeToSharedMemory(RunGuard::ipc_commands::TOGGLE_MUTE);
-        qDebug() << "Another instance is already running!";
-        return 1;
-    }
-
     signal(SIGTERM, handle_signals);
     signal(SIGABRT, handle_signals);
     signal(SIGINT, handle_signals);
 
-    PulseAudio pa;
-    KeyboardDriver keyboard;
-
-    qDebug() << "Main thread" << QThread::currentThreadId();
     pa.setMuteForAllInputDevices(true);
     keyboard.set_hsv(80, 255, 255, 1000);
 
-    return a.exec();
+    return app.exec();
 }
