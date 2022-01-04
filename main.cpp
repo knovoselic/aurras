5ac3c367d3bac113c562244f9ca3be68c7575d5a
Kristijan Novoselić <kristijan@codecons.com>
2022-01-04 06:42:53 +0100

    Fix F11 binding, make it work only in DEBUG build

--- main.cpp
+++ main.cpp
@@ -1,17 +1,17 @@
 #include <QCoreApplication>
-#include <QDebug>
-#include <iostream>
-#include <string.h>
 #include <QtGlobal>
 #include <QProcess>
+#include <QtConcurrent/QtConcurrent>
 
 #include "pulseaudio.h"
 #include "keyboarddriver.h"
-#include "xhklib.h"
 
 KeyboardDriver keyboard;
 PulseAudio pa;
 bool mute = false;
+
+#ifdef QT_DEBUG
+#include "xhklib.h"
 
 void keyboard_shortcut_pressed(xhkEvent e, void *a1, void *a2, void *a3) {
     Q_UNUSED(e);
@@ -47,32 +47,31 @@ void keyboard_shortcut_pressed(xhkEvent e, void *a1, void *a2, void *a3) {
     gdbus.setArguments(arguments);
     gdbus.startDetached();
 }
+#endif
 
 int main(int argc, char *argv[])
 {
+    QCoreApplication a(argc, argv);
+
     qDebug() << "Main thread" << QThread::currentThreadId();
     pa.setMuteForAllInputDevices(true);
     keyboard.set_hsv(80, 255, 255, 1000);
 
-#if 0
+#ifdef QT_DEBUG
     // this is just used for easier testing
-    Q_UNUSED(argc);
-    Q_UNUSED(argv);
+    QtConcurrent::run([&]{
+        xhkConfig *hk_config = xhkInit(NULL);
+        xhkBindKey(hk_config, 0, XK_F11, 0, xhkKeyPress, &keyboard_shortcut_pressed, 0, 0, 0);
 
-    xhkConfig *hk_config = xhkInit(NULL);
-    xhkBindKey(hk_config, 0, XK_F11, 0, xhkKeyPress, &keyboard_shortcut_pressed, 0, 0, 0);
+        // get all recording devices into a known state (muted)
+        keyboard_shortcut_pressed(xhkEvent(), NULL, NULL, NULL);
 
-    // get all recording devices into a known state (muted)
-    keyboard_shortcut_pressed(xhkEvent(), NULL, NULL, NULL);
-
-    while (1) {
-        xhkPollKeys(hk_config, 1);
-    }
-
-    xhkClose(hk_config);
-    return 0;
-#else
-    QCoreApplication a(argc, argv);
-    return a.exec();
+        while (1) {
+            xhkPollKeys(hk_config, 1);
+        }
+        xhkClose(hk_config);
+    });
 #endif
+
+    return a.exec();
 }
