7f8e2d0a49615a1084a175fb6e165a661a7a40f6
Kristijan Novoselić <kristijan@codecons.com>
2022-01-05 07:07:27 +0100

    Finish all of the muting logic - finally ready for v1.0!
    When the driver starts, it will mute all of the input devices. If no
    programs are using any input devices, the driver will ignore toggle
    requests and briefly flash "muted" color on the keyboard. Once a program
    starts using an input device, the driver will permanently change
    keyboard color to reflect state of the mute (red for muted, green for
    unmuted). In this state the driver will process toggle requests.
    After the program is done using the input device, the driver will revert
    into initial state - it will mute all input devices and ignore any
    toggle requests. It will also stop reporting mute state to the keyboard.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <QCoreApplication>
 #include <QtGlobal>
+#include <QTimer>
 #include <QProcess>
 #include <QtConcurrent/QtConcurrent>
 #include <csignal>
@@ -13,11 +14,13 @@
 #else
 #define SHARED_MEMORY_KEY "Aurras driver shared memory"
 #endif
+#define STATE_UPDATE_INTERVAL 250
 
 bool mute = true;
 bool anyInputDeviceActive = false;
 PulseAudio *pa;
 KeyboardDriver *keyboard;
+QTimer *updateKeyboardIndicatorTimer;
 
 void handleSignals(int signal) {
     Q_UNUSED(signal);
@@ -25,11 +28,26 @@ void handleSignals(int signal) {
     qApp->quit();
 }
 
+void updateKeyboardIndicator() {
+    if (mute) {
+        keyboard->setHsv(0, 255, 255, STATE_UPDATE_INTERVAL * 2);
+    } else {
+        keyboard->setHsv(80, 255, 255, STATE_UPDATE_INTERVAL * 2);
+    }
+}
+
+void updateMute() {
+    pa->setMuteForAllInputDevices(mute);
+    updateKeyboardIndicator();
+}
+
 void handleIPCCommand(SimpleIPC::ipc_command command) {
     switch (command) {
     case SimpleIPC::ipc_command::IPC_COMMAND_TOGGLE_MUTE:
-        mute = !mute;
-        pa->setMuteForAllInputDevices(mute);
+        if (anyInputDeviceActive) {
+            mute = !mute;
+        }
+        updateMute();
         break;
     default:
         qWarning() << "Received unexpected IPC command:" << command;
@@ -39,7 +57,17 @@ void handleIPCCommand(SimpleIPC::ipc_command command) {
 
 void handleActiveSourceOutputCountChanged(int new_count) {
     anyInputDeviceActive = new_count > 0;
+    if (anyInputDeviceActive) {
+        updateKeyboardIndicatorTimer->start();
+    } else {
+        updateKeyboardIndicatorTimer->stop();
+        mute = true;
+        updateMute();
+    }
+
+    updateKeyboardIndicator();
 }
+
 
 int client_main(SimpleIPC &guard, QCoreApplication &app) {
     QCommandLineParser parser;
@@ -65,22 +93,25 @@ int client_main(SimpleIPC &guard, QCoreApplication &app) {
     return 1;
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     QCoreApplication app(argc, argv);
     SimpleIPC ipc(SHARED_MEMORY_KEY);
 
     app.setApplicationName("Aurras");
-    app.setApplicationVersion("v0.1");
+    app.setApplicationVersion("v1.0");
     if (!ipc.initializeDaemon()) {
         return client_main(ipc, app);
     }
 
     pa = new PulseAudio();
     keyboard = new KeyboardDriver();
+    updateKeyboardIndicatorTimer = new QTimer();
+
+    updateKeyboardIndicatorTimer->setInterval(STATE_UPDATE_INTERVAL);
 
     QObject::connect(&ipc, &SimpleIPC::commandReceived, handleIPCCommand);
-    QObject::connect(pa, &PulseAudio::activeSourceOutputCountChanged, &handleActiveSourceOutputCountChanged);
+    QObject::connect(pa, &PulseAudio::activeSourceOutputCountChanged, handleActiveSourceOutputCountChanged);
+    QObject::connect(updateKeyboardIndicatorTimer, &QTimer::timeout, updateKeyboardIndicator);
 
     pa->updateSourceOutputCount();
 
@@ -88,13 +119,13 @@ int main(int argc, char *argv[])
     signal(SIGABRT, handleSignals);
     signal(SIGINT, handleSignals);
 
-    pa->setMuteForAllInputDevices(mute);
-    keyboard->setHsv(80, 255, 255, 1000);
+    updateMute();
 
     int returnValue = app.exec();
 
     delete pa;
     delete keyboard;
+    delete updateKeyboardIndicatorTimer;
 
     return returnValue;
 }
